d129728a4a1d03f212a5540a45cf75fa2e803944
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-08-06 11:15:27 -0300

    creating a api with node using express and cors

---
+++ server.js
@@ -0,0 +1,12 @@
+const cors = require('cors')
+const express = require('express');
+const app = express();
+
+app.use(cors())
+app.get('/', (request, response)=>{
+    response.json({
+        message: 'You did it! You make a call do a node server!',
+        by: 'Pablo Rodrigo'
+    })
+})
+app.listen('3000')
