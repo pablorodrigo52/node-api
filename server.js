b6fc3c28d12a41cdd7ac15901a1133d5de9690b7
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-08-25 22:50:50 -0300

    generating some list

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ const app = express();
 
 app.use(cors())
 app.get('/', (request, response)=>{
-    response.json({
-        message: 'You did it! You make a call do a node server!',
-        by: 'Pablo Rodrigo'
-    })
+    const table = {
+        header: ["#", "Nome", "Cidade", "Estado"],
+        body: [
+            ["1", "Josefina da Silva", "São Paulo", "SP"],
+            ["2", "Felipe de Moraes", "Aracaju", "SE"],
+            ["3", "João Paulo", "Aracaju", "SE"],
+            ["4", "Manoel Messias", "Copacabana", "RJ"],
+            ["5", "João Neto", "Olinda", "Pernambuco"],
+            ["6", "Aparecida Ferreira", "Campinas", "SP"],
+            ["7", "Luiz Arthur", "Belo Horizonte", "MG"],
+            ["8", "Lohany Perpetuo", "Salvador", "BA"],
+            ["9", "Terry Jones", "Gramado", "RS"],
+            ["10", "Dava Jones", "Capela", "SE"]
+        ]
+    }
+
+    response.json(table)
 })
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
